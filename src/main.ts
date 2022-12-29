7ba0a0d2c04b799e0f903c80e9aa8bfc188aaa4f
Malik <79022015+MalikN95@users.noreply.github.com>
2022-12-29 09:25:16 +0300

    first

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder } from '@nestjs/swagger';
+import { SwaggerModule } from '@nestjs/swagger/dist';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+    const config = new DocumentBuilder()
+    .setTitle('WeHost')
+    .setDescription('WeHost Routes Documentation')
+    .setVersion('1.0')
+    .addBearerAuth({ type: 'http', scheme: 'bearer', bearerFormat: 'JWT' })
+    .addSecurityRequirements('bearer')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  await app.listen(3000);
+}
+bootstrap();
