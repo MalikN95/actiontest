5f60181d31e393386afa6a1d276d09d0434ad3d6
Malik <79022015+MalikN95@users.noreply.github.com>
2023-01-03 22:20:49 +0300

    test

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+import { User } from './user/entity/user.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: 5432,
+      username: 'postgres',
+      password: process.env.POSTGRES_PASSWORD,
+      database: 'postgres',
+      entities: [User],
+      synchronize: true,
+      dropSchema: true
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/user/entity/user.entity.ts
@@ -0,0 +1,36 @@
+import { Exclude } from 'class-transformer';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  Index,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('user')
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ nullable: true })
+  name: string;
+
+  @Index()
+  @Column('varchar', { length: 320, unique: true })
+  email: string;
+
+  @Column('varchar', { length: 175, nullable: true })
+  password: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @Column('varchar', { length: 175, nullable: true, default: null })
+  @Exclude()
+  passwordResetToken: string;
+
+}
